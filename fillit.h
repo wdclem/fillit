b85a3d73c88969b3fca74f241e702927f5eec4c8
Clement Cariou <clem@Clements-MacBook-Air.local>
2022-01-13 11:09:25 +0200

    test

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:28 by thule             #+#    #+#             */
-/*   Updated: 2022/01/12 18:09:01 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/13 11:08:16 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include <fcntl.h>
 # include "libft/libft.h"
 
+typedef struct tetris
+{
+	char	**position;
+	int		width;
+	int		height;
+	char	value;
+}
 
 
 #endif
--- main.c
+++ main.c
@@ -6,16 +6,31 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:23 by thule             #+#    #+#             */
-/*   Updated: 2022/01/12 18:08:25 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/13 11:03:50 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> //rmb to delete
 #include "fillit.h"
 
+char	*map;
+
+void	print;
 
 int	main(int argc, char *argv[])
 {
+	/* two variables to open file, might not need line */
+	int 	fd;
+	char	*line;
+
+	/* Check for only one file, switch printf to putstr */
+	if (argc != 2)
+	{
+		printf("Usage only with one arg\n");
+		return (1);
+	}
+	/* Use GNL or just put everything in a buffer */
 	printf("got c");
 	return (0);
 }
+
