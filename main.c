21a221eedb97ed20bcb4a3cd83c664416948203e
Thu Le <thule@Thus-MacBook-Pro-2.local>
2022-01-14 14:34:57 +0200

    Update conditions to check valid file

--- main.c
+++ main.c
@@ -6,47 +6,61 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:23 by thule             #+#    #+#             */
-/*   Updated: 2022/01/14 12:41:46 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/14 14:33:07 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> //rmb to delete
 #include "fillit.h"
 
-int	check_valid_board(int fd)
+#define NORMAL "\x1B[0m"
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define BLUE "\x1B[34m"
+#define MAGENTA "\x1B[35m"
+#define CYAN "\x1B[36m"
+#define WHITE "\x1B[37m"
+
+int check_valid_board(int fd)
 {
 	// possible max 544;
 	char buf[546];
 	int ret;
 	int index;
 	int shape;
-	
+
 	index = 0;
 	ret = read(fd, buf, 545);
-	if (ret > 544)
+	if (ret > 544 || ((ret - 1) % 21 != 18))
 		return (0);
 	shape = (ret + 2) / 21;
-	
-	while (*buf != '\0')
+	while (buf[index] != '\0')
 	{
-		if (*buf == '#' || *buf == '.' || *buf == '\n')
+		if (buf[index] == '#' || buf[index] == '.' || buf[index] == '\n')
 		{
-			//check inside;
+			if ()
+			// check inside;
+			if (buf[index] == '\n')
+			{
+				printf("%s%d%s	", GREEN, index, WHITE);
+				printf("\n");
+			}
+			else
+				printf("%d	", index);
 		}
 		else
-		{
-			// invalid if its not # or . or \n
 			return (0);
-		}
+		index++; 
 	}
 
 	return (1);
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd;
-	
+
 	if (argc != 2)
 	{
 		ft_putstr("usage: ./fillit filename\n");
@@ -54,9 +68,15 @@ int	main(int argc, char *argv[])
 	}
 	fd = open(argv[1], O_RDONLY);
 	check_valid_board(fd);
-	
 
 	close(fd);
-	
+
+	// char *buf = "....\n##..\n.#..\n.#..";
+
+	// if (*buf != '#' && *buf != '.' && *buf != '\n' && *buf != '\0')
+	// 	printf("yes");
+	// else
+	// 	printf("no");
+
 	return (0);
 }
