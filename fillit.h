7d852ccd65738c46fca07fd3bcc05578dc086a6b
Thu Le <thule@Thus-MacBook-Pro-2.local>
2022-01-14 10:47:49 +0200

    created branch

--- fillit.h
+++ fillit.h
@@ -6,13 +6,20 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:28 by thule             #+#    #+#             */
-/*   Updated: 2022/01/12 18:09:01 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/14 10:45:49 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef FILLIT_H
 # define FILLIT_H
+
+typedef struct s_tetris
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_tetris;
 
 # include <fcntl.h>
 # include "libft/libft.h"
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:23 by thule             #+#    #+#             */
-/*   Updated: 2022/01/12 18:08:25 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/14 10:40:32 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 
 int	main(int argc, char *argv[])
 {
-	printf("got c");
+	if (argc != 2)
+	{
+		ft_putstr("usage: ./fillit input_file\n");
+	}
+	
 	return (0);
 }
