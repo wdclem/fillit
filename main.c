9b48d59b864da5d463be33c1d07002ed4d833e27
Thu Le <thule@Thus-MacBook-Pro-2.local>
2022-01-17 14:49:31 +0200

    working version of making coordination for shapes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 17:42:23 by thule             #+#    #+#             */
-/*   Updated: 2022/01/15 21:55:16 by thule            ###   ########.fr       */
+/*   Updated: 2022/01/17 14:44:08 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,86 @@
 #define CYAN "\x1B[36m"
 #define WHITE "\x1B[37m"
 
-int check_valid_board(int fd)
+int	*check_piece(char **arr)
+{
+	int x = 0;
+	int y = 0;
+	int xmin = 3;
+	int ymin = 3;
+	int counter = 0;
+	int index = 0;
+	int *shape = (int *) malloc (sizeof(int) * 4);
+	
+	while (x < 4)
+	{
+		y = 0;
+		while (y < 4)
+		{
+			if (arr[x][y] == '#')
+			{
+				if (x < xmin)
+					xmin = x;
+				if (y < ymin)
+					ymin = y;
+				if (x != 0 && arr[x - 1][y] == '#')
+					counter++;
+				if (x != 3 && arr[x + 1][y] == '#')
+					counter++;
+				if (y != 0 && arr[x][y - 1] == '#')
+					counter++;
+				if (y != 3 && arr[x][y + 1] == '#')
+					counter++;
+				shape[index++] = x;
+				shape[index++] = y;
+			}
+			y++;
+		}
+		x++;
+	}
+	index = 0;
+	printf("%d %d\n", xmin, ymin);
+	while (index < 8)
+	{
+		if (index % 2 == 0)
+			shape[index] = shape[index] - xmin;
+		else
+			shape[index] = shape[index] - ymin;
+		index++;
+	}
+	printf("%d ", shape[0]);
+	printf("%d,", shape[1]);
+	printf(" %d ", shape[2]);
+	printf("%d,", shape[3]);
+	printf(" %d ", shape[4]);
+	printf("%d,", shape[5]);
+	printf(" %d ", shape[6]);
+	printf("%d", shape[7]);
+	return NULL;
+}
+
+int	make_piece(int fd, int shapes)
+{
+	char buf[22];
+	read(fd, buf, 21);
+	buf[21] = '\0';
+	char **arr = ft_strsplit(buf, '\n');
+	printf("%s\n", arr[0]);
+	printf("%s\n", arr[1]);
+	printf("%s\n", arr[2]);
+	printf("%s\n", arr[3]);
+	check_piece(arr);
+	
+	//free malloc afterward
+	ft_strdel(&arr[0]);
+	ft_strdel(&arr[1]);
+	ft_strdel(&arr[2]);
+	ft_strdel(&arr[3]);
+	ft_strdel(arr);
+
+	return (0);
+}
+
+int	check_valid_board(int fd)
 {
 	char buf[547];
 	int ret;
@@ -55,6 +134,7 @@ int check_valid_board(int fd)
 int main(int argc, char *argv[])
 {
 	int fd;
+	int shapes;
 
 	if (argc != 2)
 	{
@@ -62,11 +142,12 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 	fd = open(argv[1], O_RDONLY);
-	int res = check_valid_board(fd);
-	printf("\nresult is %d\n", res);
-
+	shapes = check_valid_board(fd);
 	close(fd);
-
+	if (!shapes)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	make_piece(fd, shapes);
 
 	return (0);
 }
