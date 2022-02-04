741a9096013400455a62e3edf03e9a4703b8e7cd
Thi anh thu Le <thle@c1r1p8.hive.fi>
2022-02-04 13:13:57 +0200

    norm fixed

--- solver.c
+++ solver.c
@@ -3,44 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   solver.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
+/*   By: thle <thle@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 15:38:45 by thle              #+#    #+#             */
-/*   Updated: 2022/02/03 15:40:01 by thule            ###   ########.fr       */
+/*   Updated: 2022/02/04 13:12:55 by thle             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	solver(char **board, int shape[][8], int amount, int index)
+static int	attempt(char **board, int shape[][8], int arr[2], int index)
 {
 	int	x;
 	int	y;
-	int	len;
 
-	len = ft_strlen(*board);
-	if (index == amount)
-		return (1);
-	while (index < amount)
+	x = 0;
+	while (x < arr[1])
 	{
-		x = 0;
-		while (x < len)
+		y = 0;
+		while (y < arr[1])
 		{
-			y = 0;
-			while (y < len)
-			{
-				if (valid_placement(board, shape[index], (int [2]){x, y}, index + 'A'))
-					if (solver(board, shape, amount, index + 1))
-						return (1);
-					else
-						removal(board, shape[index], x, y);
+			if (valid_placement(board, shape[index],
+					(int [2]){x, y}, index + 'A'))
+			{	
+				if (solver(board, shape, arr, index + 1))
+					return (1);
 				else
-					if (x == len - 1 && y == len - 1)
-						return (0);
-				y++;
+					removal(board, shape[index], x, y);
 			}
-			x++;
+			else
+				if (x == arr[1] - 1 && y == arr[1] - 1)
+					return (0);
+			y++;
 		}
+		x++;
+	}
+	return (2);
+}
+
+int	solver(char **board, int shape[][8], int arr[2], int index)
+{
+	int	res;
+
+	if (index == arr[0])
+		return (1);
+	while (index < arr[0])
+	{
+		res = attempt(board, shape, arr, index);
+		if (res == 0 || res == 1)
+			return (res);
 		index++;
 	}
 	return (0);
